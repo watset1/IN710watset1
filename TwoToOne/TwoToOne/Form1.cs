65b63e9afe7ff35ad7a9f52fd46d70482e5f3572
watset1 <watset1@student.op.ac.nz>
2016-02-16 18:28:42 +1300

    TwoToOne completed
    Copies contents of a two dimensional array into a one dimensional array
    and prints the result on a button click

---
+++ TwoToOne/TwoToOne/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TwoToOne
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void TwoToOne()
+        {
+            const int columnCount = 4;
+            const int rowCount = 2;
+
+	        Char[] oneDimensional = new Char[columnCount * rowCount];
+            Char[,] twoDimensional = new Char[columnCount, rowCount] 
+                  { { 'a', 'b' }, { 'c', 'd' }, { 'e', 'f' }, { 'g', 'h' } };
+
+            // convert to one dimension
+            for (int c = 0; c < columnCount; c++)
+                for (int r = 0; r < rowCount; r++)
+                {
+                    oneDimensional[r + c * 2] = twoDimensional[c, r];
+                }
+
+            //printing one dimensional array
+            for (int i = 0; i < columnCount * rowCount; i++)
+                listBox1.Items.Add(oneDimensional[i]);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TwoToOne();
+        }
+    }
+}
