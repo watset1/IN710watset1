ee53270b45c5c1da987e17bb56fb610124f5a19b
watset1 <watset1@student.op.ac.nz>
2016-03-10 21:15:44 +1300

    Application functioning, no testing
    Still needs testing

---
+++ Animal Shelter Skeleton/IN710 4.1 Animal Shelter Solution 2014/PictureDisplay.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace IN710_4._1_Animal_Shelter_Solution_2014
+{
+    class PictureDisplay: IDisplay
+    {
+        public List<PictureBox> pictureBoxList { get; set; }
+
+        public PictureDisplay(List<PictureBox> pictureBoxList)
+        {
+            this.pictureBoxList = pictureBoxList;
+        }
+
+        public void displayCritterList(List<Critter> critterList)
+        {
+            for (int i = 0; i < critterList.Count(); i++)
+            {
+                pictureBoxList.ElementAt(i).Image = Image.FromFile(critterList.ElementAt(i).ImageFileName);
+            }
+        }
+
+        public void clearDisplay()
+        {
+            foreach (PictureBox pictureBox in pictureBoxList)
+            {
+                pictureBox.Image = null;
+            }
+        }
+    }
+}
---
+++ Animal Shelter Skeleton/IN710 4.1 Animal Shelter Solution 2014/TextDisplay.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace IN710_4._1_Animal_Shelter_Solution_2014
+{
+    class TextDisplay: IDisplay
+    {
+        public ListBox listBox { get; set; }
+
+        public TextDisplay(ListBox listBox)
+        {
+            this.listBox = listBox;
+        }
+
+        public void displayCritterList(List<Critter> critterList)
+        {
+            foreach (Critter critter in critterList)
+            {
+                listBox.Items.Add(critter.Name);               
+            }
+        }
+
+        public void clearDisplay()
+        {
+            listBox.Items.Clear();
+        }
+    }
+}
