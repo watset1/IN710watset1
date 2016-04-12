a36b83022b25dcb660a57b2d86b4350e313f9eb7
watset1 <watset1@student.op.ac.nz>
2016-04-12 16:06:40 +1200

    Adjusted existing predicate task to use lambda expressions

---
+++ Week 9/Predicate Task Lambda/Predicate Task/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Predicate_Task
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        List<int> randomNumberList;
+
+        public Form1()
+        {
+            InitializeComponent();          
+            randomNumberList = new List<int>();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            randomNumberList.Clear();
+            for (int i = 0; i < 100; i++)
+            {
+                int randNum = random.Next(100);
+                randomNumberList.Add(randNum);
+            }
+            foreach (int num in randomNumberList)
+                listBox1.Items.Add(num.ToString());
+        }
+
+        private void btnEvens_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            foreach (int num in randomNumberList.FindAll(x => x % 2 == 0))
+                listBox2.Items.Add(num.ToString());
+        }
+  
+        private void btnUnderTen_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            foreach (int num in randomNumberList.FindAll(x => x < 10))
+                listBox2.Items.Add(num.ToString());
+        }       
+    }
+}
